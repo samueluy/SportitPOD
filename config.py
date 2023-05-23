36c0e35a0e6dba7c22f3b0adea7b966ab1358a57
samueluy <samuel_uy@dlsu.edu.ph>
2023-05-23 22:04:16 +0800

    Cred setup

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-client_id='Vl07WV3YGrXMMzH996lXlA',
-client_secret='WmL5oJ5vWbmsB8AIKmVeLdSQ3ZbWog',
+client_id='YOUR_CLIENT_ID',
+client_secret='YOUR_CLIENT_SECRET',
 user_agent='Sportsbook POTD posts request'
 
 subreddit_name = 'sportsbook'
