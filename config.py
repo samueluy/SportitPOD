a6b41b2924586ed9b2393854ef756e9891d4a4e6
samueluy <samuel_uy@dlsu.edu.ph>
2023-05-23 21:55:45 +0800

    Added .gitignore for config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-client_id='YOUR_CLIENT_ID',
-client_secret='YOUR_CLIENT_SECRET',
+client_id='Vl07WV3YGrXMMzH996lXlA',
+client_secret='WmL5oJ5vWbmsB8AIKmVeLdSQ3ZbWog',
 user_agent='Sportsbook POTD posts request'
 
 subreddit_name = 'sportsbook'
