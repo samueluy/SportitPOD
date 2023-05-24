7e46587ee3043249ff1c78cbe78f16b06244faef
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-05-24 08:18:18 +0800

    Limited only to root comments

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-client_id='YOUR_CLIENT_ID',
-client_secret='YOUR_CLIENT_SECRET',
-user_agent='Sportsbook POTD posts request'
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
+user_agent = "Sportsbook POTD posts request"
 
-subreddit_name = 'sportsbook'
-flair_name = 'POTD 🧹'
+subreddit_name = "sportsbook"
+flair_name = "POTD 🧹"
--- posts.py
+++ posts.py
@@ -5,9 +5,9 @@ import config
 
 # Initialize the Reddit API client
 reddit = praw.Reddit(
-    client_id= config.client_id,
-    client_secret= config.client_secret,
-    user_agent= config.user_agent
+    client_id=config.client_id,
+    client_secret=config.client_secret,
+    user_agent=config.user_agent,
 )
 
 # Define the subreddit you want to search in
@@ -20,7 +20,9 @@ subreddit = reddit.subreddit(subreddit_name)
 flair_name = config.flair_name
 
 # Get the posts with the specified flair in the subreddit
-newest_post = subreddit.search(f'flair_name:"{flair_name}"', sort='new', time_filter='day')
+newest_post = subreddit.search(
+    f'flair_name:"{flair_name}"', sort="new", time_filter="day"
+)
 
 comments = []
 
@@ -28,13 +30,24 @@ comments = []
 # Iterate over the posts and retrieve the comments
 for post in newest_post:
     try:
-        print(f'Post title: {post.title}')
-        print('Comments:')
+        print(f"Post title: {post.title}")
+        print("Comments:")
         post.comments.replace_more(limit=None)
         for comment in post.comments.list():
-            print(f'Comment: {comment.body}')
-            print(f'Upvotes: {comment.score}')
-            print('---')
+            if (
+                comment.author and comment.is_root
+            ):  # has author and is not a reply to a comment
+                com = {
+                    "author": comment.author,
+                    "body": comment.body,
+                    "upvotes": comment.score,
+                }
+                comments.append(com)
+                # print(f"Comment: {comment.body}")
+                # print(f"Upvotes: {comment.score}")
+                # print("---")
     except:
         print("End...")
-        #print(comments)
+        print(comments)
+        # print(comments)
+print(comments)
