a50a8144aab7cba0bdcecb4cf116f2090def9588
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-05-26 14:32:13 +0800

    added navbar and logo

--- sportitpod_frontend/src/App.js
+++ sportitpod_frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Comments from "./components/Comment";
 import Timer from "./components/Timer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Comments />
       <Timer />
     </div>
