a66aac6932d3bc8acc230c71378a869aa2eac4b4
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-05-27 11:51:48 +0800

    Use of props

--- sportitpod_frontend/src/App.js
+++ sportitpod_frontend/src/App.js
@@ -2,12 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Comments from "./components/Comment";
+import data from "./comments.json";
 
 function App() {
+  const comments = data.map((item) => {
+    return (
+      <Comments author={item.author} body={item.body} upvotes={item.upvotes} />
+    );
+  });
+
   return (
     <div className="App">
       <Navbar />
-      <Comments />
+      {comments}
     </div>
   );
 }
--- sportitpod_frontend/src/components/Comment.js
+++ sportitpod_frontend/src/components/Comment.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-export default function (props) {
+export default function Comments(props) {
   return (
     <div className="comment">
       <div className="comment--meta">
-        <span className="comment--upvotes">32 </span>
+        <span className="comment--upvotes">{props.upvotes}</span>
         <span className="comment--spacer"> </span>
-        <p className="comment--author">sbpotdbot</p>
+        <p className="comment--author">{props.author}</p>
       </div>
-      <ReactMarkdown className="comment--body">
-        **If you choose to self promote (tips excluded) in the POTD thread:**
-        You must have accurate tracking of your full POTD record with detailed
-        stats including **ROI+average odds+units won** written into the comment.
-        No resetting records. **|** For picks that do not fit the POTD rules use
-        the Daily Discussion posts.
-      </ReactMarkdown>
+      <ReactMarkdown className="comment--body">{props.body}</ReactMarkdown>
     </div>
   );
 }
