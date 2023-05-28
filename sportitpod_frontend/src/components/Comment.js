abaedcda5da256ddae6d1d120ed7a46359fc6dbb
Samuel Uy <samuel_uy@dlsu.edu.ph>
2023-05-28 17:53:45 +0800

    integrated with django

--- sportitpod_frontend/src/components/Comment.js
+++ sportitpod_frontend/src/components/Comment.js
@@ -5,9 +5,10 @@ export default function Comments(props) {
   return (
     <div className="comment">
       <div className="comment--meta">
-        <span className="comment--upvotes">{props.upvotes}</span>
-        <span className="comment--spacer"> </span>
-        <p className="comment--author">{props.author}</p>
+        <div className="comment--meta-box">
+          <span className="comment--upvotes">{props.upvotes}</span>
+          <p className="comment--author">{props.author}</p>
+        </div>
       </div>
       <ReactMarkdown className="comment--body">{props.body}</ReactMarkdown>
     </div>
--- sportitpod_frontend/src/components/Navbar.js
+++ sportitpod_frontend/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Timer from "./Timer";
+import logo from "../logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="./logo.png" className="nav--logo" />
+      <img src={logo} className="nav--logo" />
       <Timer />
     </nav>
   );
